47126c938135d3c1a3d3d4f5c3479d1f51add6c1
Alexander Simpson <as2388@cam.ac.uk>
2023-06-15 11:58:34 +0100

    Add standard run scripts

---
+++ docker-run-log-pipeline-event.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="$(<configurations/docker_image_name.txt)"
+
+# Check that the correct number of arguments were provided.
+if [[ $# -ne 5 ]]; then
+    echo "Usage: ./docker-run-log-pipeline-event.sh
+    [--profile-cpu <cpu-profile-output-path>] <configuration-file> <code-schemes-dir>
+     <google-cloud-credentials-file-path> <run-id> <event-key>"
+    echo "Updates pipeline event/status to a firebase table to aid in monitoring"
+    exit 1
+fi
+
+# Assign the program arguments to bash variables.
+CONFIGURATION_FILE=$1
+CODE_SCHEMES_DIR=$2
+GOOGLE_CLOUD_CREDENTIALS_PATH=$3
+RUN_ID=$4
+EVENT_KEY=$5
+
+CMD="pipenv run python -u log_pipeline_event.py configuration /credentials/google-cloud-credentials.json \
+       ${RUN_ID} ${EVENT_KEY}"
+
+container="$(docker container create -w /app "$IMAGE_NAME" /bin/bash -c "$CMD")"
+
+echo "Created container $container"
+container_short_id=${container:0:7}
+
+# Copy input data into the container
+echo "Copying $GOOGLE_CLOUD_CREDENTIALS_PATH -> $container_short_id:/credentials/google-cloud-credentials.json"
+docker cp "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$container:/credentials/google-cloud-credentials.json"
+
+echo "Copying $CODE_SCHEMES_DIR -> $container_short_id:/app/code_schemes"
+docker cp "$CODE_SCHEMES_DIR" "$container:/app/code_schemes"
+
+echo "Copying $CONFIGURATION_FILE -> $container_short_id:/app/configuration.py"
+docker cp "$CONFIGURATION_FILE" "$container:/app/configuration.py"
+
+# Run the container
+echo "Starting container $container_short_id"
+docker start -a -i "$container"
+
+# Tear down the container when it has run successfully
+docker container rm "$container" >/dev/null
---
+++ run_pipeline.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# -ne 7 ]]; then
+    echo "Usage: ./run_pipeline.sh"
+    echo "<user> <pipeline-name> <google-cloud-credentials-file-path> <configuration-file> <code-schemes-dir> <data-dir> <archive-dir>"
+    echo "Runs the pipeline end-to-end (sync-rapid-pro-to-engagement-db, sync-engagement-db-to-coda, sync-coda-to-engagement-db,\
+          sync-engagement-db-to-rapid-pro, run-engagement-db-to-analysis, ARCHIVE)"
+    exit
+fi
+
+USER=$1
+PIPELINE_NAME=$2
+GOOGLE_CLOUD_CREDENTIALS_PATH=$3
+CONFIGURATION_FILE=$4
+CODE_SCHEMES_DIR=$5
+DATA_DIR=$6
+ARCHIVE_DIR=$7
+
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+HASH=$(git rev-parse HEAD)
+RUN_ID="$DATE-$HASH"
+ARCHIVE_FILE="$ARCHIVE_DIR/data-$RUN_ID.tar.gzip"
+
+echo "Starting a new pipeline run with id ${RUN_ID}"
+
+./docker-run-log-pipeline-event.sh \
+    "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$RUN_ID" "PipelineRunStart"
+
+./docker-sync-rapid-pro-to-engagement-db.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-rapid-pro-to-engagement-db-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./docker-sync-csvs-to-engagement-db.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-csv-to-engagement-db-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./docker-sync-google-forms-to-engagement-db.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-google-forms-to-engagement-db-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./docker-sync-engagement-db-to-coda.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-coda-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./docker-sync-coda-to-engagement-db.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-coda-to-engagement-db-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./docker-sync-engagement-db-to-rapid-pro.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-rapid-pro-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR"
+
+./docker-run-engagement-db-to-analysis.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-analysis-cache" \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
+./archive_data_dir.sh "$DATA_DIR" "$ARCHIVE_FILE"
+
+./docker-run-upload-archive-files.sh \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$ARCHIVE_DIR"
+
+./docker-run-log-pipeline-event.sh \
+    "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$RUN_ID" "PipelineRunEnd"
